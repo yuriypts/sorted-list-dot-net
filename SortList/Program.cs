5ac523206d512ab60059cc0fa9efc5a0e7b66f9c
Yuriy Parashynets <yura.parashynets@gmail.com>
2019-09-22 21:55:28 +0300

    test 123
    test

---
+++ SortList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SortList
+{
+    class Program
+    {
+        static int[] arr = new int[] { 1, 6, 3, 9, 8, 5 };
+        //static int[] arr = new int[] { 6, 3, 9, 4, 2 };
+
+        static void Main(string[] args)
+        {
+            SortedList sortList = new SortedList();
+
+            Console.WriteLine("Unsorted List");
+
+            for (int i = 0; i < arr.Length; i++) 
+            {
+                Console.WriteLine("number is - {0}", arr[i]);
+            }
+
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine("Sorted List");
+
+            int[] sortedList = sortList.SortedListByAsc(arr);
+
+            for (int numS = 0; numS < sortedList.Length; numS++)
+            {
+                Console.WriteLine("number is - {0}", sortedList[numS]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SortList/SortedList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortList
+{
+    public class SortedList
+    {
+        private int[] customArr;
+
+        public int[] SortedListByAsc(int[] arr)
+        {
+            if (arr == null || arr.Length < 1)
+                return arr;
+
+            int[] sortedArr = new int[arr.Length];
+            customArr = arr.Clone() as int[];
+
+            for (int num = 0; num < arr.Length; num++)
+            {
+                int value = customArr[0];
+
+                for (int q = 0; q < customArr.Length; q++)
+                {
+                    if (customArr[q] < value)
+                    {
+                        value = customArr[q];
+                    }
+                }
+
+                customArr = Array.FindAll(customArr, x => x != value);
+
+                sortedArr[num] = value;
+            }
+
+            return sortedArr;
+        }
+    }
+}
